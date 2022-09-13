9dc0fb00a480ae78ceab3d7731b6e8d58b69f1f6
Junaid Malik <junmalik109@gmail.com>
2022-09-13 10:35:49 +0100

    added basic component for Login page

---
+++ components/Login.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Image
+ from 'next/image'
+function Login() {
+  return (
+    <div className='bg-black relative '>
+    
+        <h1>Login Screen </h1>
+
+        <div className='flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4'>
+            <Image className='object-cover rounded-full' src="https://links.papareact.com/t4i" layout="fill" height={200} width={200} />
+            
+            <button className='bg-blue-500 p-5 rounded-full text-white font-bold animate-pulse'>Login with Metamask</button>
+        </div>
+
+
+
+        <div className='w-full h-screen'>
+            <Image
+                src="https://links.papareact.com/55n"
+                layout='fill'
+                objectFit='cover'
+            
+            />
+
+        </div>
+    </div>
+
+       
+    
+
+  )
+}
+
+export default Login
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
 import xw from 'xwind'
 import ButtonReact from '../components/ButtonReact'
 import ButtonStyled from '../components/ButtonStyled'
+import Login from '../components/Login'
 
-const Index = () => (
-  <div css={xw`grid justify-center items-center h-screen space-y-20`}>
+function Index() {
+
+  const isAuthenticated = false;
+
+  if(isAuthenticated) 
+  {
+    return <Login></Login>
+  }
+  <div className='h-screen' css={xw`grid justify-center items-center h-screen space-y-20`}>
     <div css={xw`space-y-20`}>
+    <h1>Metaverse React</h1>
       <ButtonReact>@emotion/react</ButtonReact>
       <ButtonStyled>@emotion/styled</ButtonStyled>
     </div>
-  </div>
-)
+  </div>  
+}
 
 export default Index
