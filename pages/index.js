458fe0ecf604a672119f32f4e38064568b026e88
Junaid Malik <junmalik109@gmail.com>
2023-01-15 17:06:16 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,22 +6,31 @@ import '../styles/globals.css'
 
 function Index() {
 
-  const { authenticate, isAuthenticated, isAuthenticating, user } = useMoralis();
+  const {isAuthenticated, logout} = useMoralis();
 
-  if(isAuthenticated) 
+  if(!isAuthenticated) return <Login />;
+
+  return
   {
-
-    return <Login>
-    </Login>
-
-  }
-  <div className='h-screen' css={xw`grid justify-center items-center h-screen space-y-20`}>
-    <div css={xw`space-y-20`}>
+    <div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-900 overflow-hidden">
+  <Head className='h-screen' css={xw`grid justify-center items-center h-screen space-y-20`}>
+    <div css={xw`space-y-20`} >
     <h1>Metaverse React</h1>
       <ButtonReact>@emotion/react</ButtonReact>
       <ButtonStyled>@emotion/styled</ButtonStyled>
     </div>
-  </div>  
+  </Head>  
+
+  <div className="h-screen overflow-y-scroll">
+    <div className="max-w-screen-2xl">
+    <Header></Header>
+
+    </div>
+  </div>
+  <ButtonStyled onClick={logout}></ButtonStyled>
+  </div>
+
+  }
 }
 
 export default Index
