44ed677f05ca4aa54edf9b639d40ccb071ceef2c
Junaid Malik <junmalik109@gmail.com>
2023-01-15 17:05:38 +0000

    Update Login.js

--- components/Login.js
+++ components/Login.js
@@ -12,27 +12,23 @@ function Login() {
         <h1>Login Screen </h1>
 
         <div className='flex flex-col absolute z-50 h-4/6 w-full items-center justify-center space-y-4'>
-            <Image className='object-cover rounded-full' src="https://links.papareact.com/t4i" layout="fill" height={200} width={200} />
+            <Image className='object-cover rounded-full' src="https://links.papareact.com/3pi" layout="fill" height={200} width={200} />
             
             <button onClick={() => authenticate} className='bg-blue-500 p-5 rounded-full text-white font-bold animate-pulse'>Login with Metamask</button>
+            
         </div>
-
-
 
         <div className='w-full h-screen'>
             <Image
                 src="https://links.papareact.com/55n"
                 layout='fill'
-                objectFit='cover'
-            
-            />
+                objectFit='cover'           
+            /> 
+
+
 
         </div>
     </div>
-
-       
-    
-
   )
 }
 
